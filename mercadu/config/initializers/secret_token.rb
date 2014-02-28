1e760617d8c67e0862a87999effd289de9f7c6af
m.flanders <mflanders82@gmail.com>
2014-02-28 00:05:59 -0500

    initial commit mercadu

---
+++ mercadu/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mercadu::Application.config.secret_key_base = 'daa494eb026df59d5c2c0b5009c4301e1bfb3c29c75ad0fdb214b956c802466ce80df0f6e3cbd31914173920120ee1daf641cf33af275b96d31710512a642e7a'
